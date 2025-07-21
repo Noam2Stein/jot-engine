d54d7150e475e6c805ba8c7c2c643d1ff144f6a0
Noam2Stein <noamsteindev@gmail.com>
2025-07-21 15:22:37 +0300

    trigger & button fused

--- jot_scheme/src/button.rs
+++ jot_scheme/src/button.rs
@@ -1,3 +1,5 @@
+use std::mem::replace;
+
 use super::*;
 
 use bitvec::array::BitArray;
@@ -5,6 +7,7 @@ use private::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub struct Button {
+    pub is_triggered: bool,
     pub is_held: bool,
 }
 
@@ -16,10 +19,19 @@ impl InputState for Button {
         ButtonResolver {
             bindings: bindings.to_map(),
             held_bindings: BitArray::default(),
+            is_triggered: false,
         }
     }
 
     fn event(resolver: &mut Self::ResolverState, event: &InputDeviceEvent) {
+        fn set_binding(resolver: &mut ButtonResolver, binding_idx: usize, is_held: bool) {
+            if is_held && !resolver.held_bindings[binding_idx] {
+                resolver.is_triggered = true;
+            }
+
+            resolver.held_bindings.set(binding_idx, is_held);
+        }
+
         match event {
             InputDeviceEvent::Connect => {}
             InputDeviceEvent::Disconnect => {}
@@ -30,28 +42,48 @@ impl InputState for Button {
                 ..
             }) => {
                 if let Some(binding_idx) = resolver.bindings.get(&FlatBinding::Key(*key)) {
-                    resolver.held_bindings.set(*binding_idx, state.is_pressed());
+                    set_binding(resolver, *binding_idx, state.is_pressed());
                 }
             }
 
             InputDeviceEvent::Mouse(MouseEvent::Button { button, state }) => {
                 if let Some(binding_idx) = resolver.bindings.get(&FlatBinding::MouseButton(*button))
                 {
-                    resolver.held_bindings.set(*binding_idx, state.is_pressed());
+                    set_binding(resolver, *binding_idx, state.is_pressed());
+                }
+            }
+
+            InputDeviceEvent::Mouse(MouseEvent::Scroll(scroll_delta)) => {
+                if scroll_delta.x() > 0.0 {
+                    if let Some(_) = resolver.bindings.get(&FlatBinding::MouseScrollRight) {
+                        resolver.is_triggered = true;
+                    }
+                } else if scroll_delta.x() < 0.0 {
+                    if let Some(_) = resolver.bindings.get(&FlatBinding::MouseScrollLeft) {
+                        resolver.is_triggered = true;
+                    }
+                }
+
+                if scroll_delta.y() > 0.0 {
+                    if let Some(_) = resolver.bindings.get(&FlatBinding::MouseScrollUp) {
+                        resolver.is_triggered = true;
+                    }
+                } else if scroll_delta.y() < 0.0 {
+                    if let Some(_) = resolver.bindings.get(&FlatBinding::MouseScrollDown) {
+                        resolver.is_triggered = true;
+                    }
                 }
             }
 
             InputDeviceEvent::Gamepad(GamepadEvent::Button { button, state }) => {
                 if let Some(binding_idx) = resolver.bindings.get(&FlatBinding::Button(*button)) {
-                    resolver.held_bindings.set(*binding_idx, state.is_pressed());
+                    set_binding(resolver, *binding_idx, state.is_pressed());
                 }
             }
 
             InputDeviceEvent::Gamepad(GamepadEvent::Value { code, value }) => {
                 if let Some(binding_idx) = resolver.bindings.get(&FlatBinding::Value(*code)) {
-                    resolver
-                        .held_bindings
-                        .set(*binding_idx, *value > u8::MAX / 2);
+                    set_binding(resolver, *binding_idx, *value > u8::MAX / 2);
                 }
             }
 
@@ -62,6 +94,7 @@ impl InputState for Button {
 
     fn step(resolver: &mut Self::ResolverState) -> Self {
         Self {
+            is_triggered: replace(&mut resolver.is_triggered, false),
             is_held: resolver.held_bindings.any(),
         }
     }
@@ -78,5 +111,6 @@ mod private {
     pub struct ButtonResolver {
         pub(super) bindings: HashMap<FlatBinding, usize>,
         pub(super) held_bindings: BitArray<[u64; 1]>,
+        pub(super) is_triggered: bool,
     }
 }
