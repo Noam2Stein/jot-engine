15a895a99d60eef61d476b364effde6d1d4d871a
Noam2Stein <noamsteindev@gmail.com>
2025-07-22 20:24:21 +0300

    rename empty game example

--- examples/dodger/src/main.rs
+++ examples/dodger/src/main.rs
@@ -1,3 +1,15 @@
+use jot::game::{Game, run};
+
 fn main() {
-    println!("Hello, world!");
+    run::<EmptyGame>();
+}
+
+struct EmptyGame {}
+
+impl Game for EmptyGame {
+    const NAME: &str = "Empty Game";
+
+    fn new(_gpu: &jot::gpu::Gpu) -> Self {
+        Self {}
+    }
 }
