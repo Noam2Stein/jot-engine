0f3b7cb3fd0e9259165c919f577fe4b3f7889073
Noam2Stein <noamsteindev@gmail.com>
2025-07-26 10:32:14 +0300

    game folder structure

--- jot_game/src/lib.rs
+++ jot_game/src/lib.rs
@@ -4,45 +4,8 @@ use jot_math::*;
 use jot_window::*;
 
 mod event;
+mod game;
 mod run_;
 pub use event::*;
+pub use game::*;
 pub use run_::*;
-
-pub trait Game {
-    const NAME: &str;
-
-    fn surface_desc() -> GpuSurfaceDesc {
-        Default::default()
-    }
-
-    fn new(_gpu: &Gpu) -> Self;
-
-    fn update(&mut self, _delta_time: f64, _gpu: &Gpu) -> GameFlow {
-        GameFlow::Continue
-    }
-
-    fn event(&mut self, event: &GameEvent, _gpu: &Gpu) -> GameFlow {
-        event.into()
-    }
-
-    fn draw(&mut self, output: &GpuTexture<2>, gpu: &Gpu) {
-        output.clear(Some(vec4!(0.0, 0.0, 0.0, 0.0)), None, gpu);
-    }
-}
-
-#[must_use]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
-pub enum GameFlow {
-    #[default]
-    Continue,
-    Exit,
-}
-
-impl From<&GameEvent> for GameFlow {
-    fn from(value: &GameEvent) -> Self {
-        match value {
-            GameEvent::ExitRequested => Self::Exit,
-            _ => Self::Continue,
-        }
-    }
-}
